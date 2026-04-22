2b9f47567a89f0199cbff48b31322d4d801c0d35
qerib-seferli <seferovqerib09@gmail.com>
2026-04-23 03:28:09 +0400

    Update supabase.js

--- assets/js/supabase.js
+++ assets/js/supabase.js
@@ -3,8 +3,8 @@
 // Yalnız public anon key frontend-də istifadə olunur
 // ======================================================
 
-const SUPABASE_URL = "https://acafrpfpzquyjpkqlhzm.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFjYWZycGZwenF1eWpwa3FsaHptIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY3ODg1NzAsImV4cCI6MjA5MjM2NDU3MH0.c5sh_iC7qozS3k5g3vYgxCDBMhF1AdSt7VkNPwr3KEU";
+const SUPABASE_URL = "https://ozrcfjibufzfmvydccst.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im96cmNmamlidWZ6Zm12eWRjY3N0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY1NDcxMTIsImV4cCI6MjA5MjEyMzExMn0.rc9hmJm1DwltaI4VDOOsyIuYPT9Lod-xXWg7mf6QwWw";
 
 const supabaseClient = window.supabase.createClient(
   SUPABASE_URL,
